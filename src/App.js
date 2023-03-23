6bdb102e53b71abc0a437d8a854ccbcdcf53ddcc
Fernando Cardoso Magaña <fernando.cardoso@scio.local>
2023-03-23 17:54:42 -0600

    ya casi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import useRandomUser from './components/useRandomUser';
 
 function App() {
+  const apiUrl = 'https://randomuser.me/api/?nat=mx';
+  const data = useRandomUser(apiUrl);
+  const name = data.name;
+  //const name = data.results[0].name.first;
+  
+  /*const getRandomUser = () => {
+    const apiUrl = 'https://randomuser.me/api';
+    const [user, setUser] = useState();
+
+    const fetchApi = async () => {
+      const response = await fetch(apiUrl);
+      const responseJSON = await response.json();
+      setUser(responseJSON);
+      console.log(responseJSON);
+      console.log(user.results[0].name.first)
+    }
+    useEffect(() => {
+      fetchApi();
+    }, []);
+  }*/
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>this name: {name}</p>
     </div>
   );
 }
